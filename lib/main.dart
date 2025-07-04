95cefb647805a0d878db03c6dfccd5eabcbd94cf
Cestel <cestel@MACDev.local>
2025-07-04 12:04:24 +0200

    prepara ios

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'screens/inicio_page.dart';
 
 void main() {
+const apiUrl1 = String.fromEnvironment('DART_DEFINE_API_URL', defaultValue: 'NO DEFINIDO');
+  const apiUrl = String.fromEnvironment('API_URL', defaultValue: 'NO DEFINIDO');
+  print('🌐 API_URL activo: $apiUrl');
+    print('🌐 API_URL1 activo: $apiUrl1');
   runApp(const FondosViajerosApp());
 }
+
 
 class FondosViajerosApp extends StatelessWidget {
   const FondosViajerosApp({super.key});
